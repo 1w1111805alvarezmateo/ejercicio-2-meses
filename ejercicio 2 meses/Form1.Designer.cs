7e99b4aad7746cb58f3a6aee7f3fccb3bedc0467
1w1111805alvarezmateo <69877204+1w1111805alvarezmateo@users.noreply.github.com>
2020-08-31 16:57:50 -0300

    Initial commit

---
+++ ejercicio 2 meses/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ejercicio_2_meses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMeses = new System.Windows.Forms.ListBox();
+            this.lstSlectMeses = new System.Windows.Forms.ListBox();
+            this.btnQuitarAll = new System.Windows.Forms.Button();
+            this.btnQitarSelec = new System.Windows.Forms.Button();
+            this.btnAgregarAll = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAgregarSelec = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstMeses
+            // 
+            this.lstMeses.AccessibleName = "";
+            this.lstMeses.FormattingEnabled = true;
+            this.lstMeses.Location = new System.Drawing.Point(12, 35);
+            this.lstMeses.Name = "lstMeses";
+            this.lstMeses.Size = new System.Drawing.Size(118, 212);
+            this.lstMeses.TabIndex = 0;
+            this.lstMeses.SelectedIndexChanged += new System.EventHandler(this.lstMeses_SelectedIndexChanged);
+            // 
+            // lstSlectMeses
+            // 
+            this.lstSlectMeses.AccessibleName = "lstMesesSlec";
+            this.lstSlectMeses.FormattingEnabled = true;
+            this.lstSlectMeses.Location = new System.Drawing.Point(277, 35);
+            this.lstSlectMeses.Name = "lstSlectMeses";
+            this.lstSlectMeses.Size = new System.Drawing.Size(118, 212);
+            this.lstSlectMeses.TabIndex = 0;
+            // 
+            // btnQuitarAll
+            // 
+            this.btnQuitarAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitarAll.Location = new System.Drawing.Point(175, 187);
+            this.btnQuitarAll.Name = "btnQuitarAll";
+            this.btnQuitarAll.Size = new System.Drawing.Size(62, 23);
+            this.btnQuitarAll.TabIndex = 1;
+            this.btnQuitarAll.Text = "<<";
+            this.btnQuitarAll.UseVisualStyleBackColor = true;
+            this.btnQuitarAll.Click += new System.EventHandler(this.btnQuitarAll_Click);
+            // 
+            // btnQitarSelec
+            // 
+            this.btnQitarSelec.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQitarSelec.Location = new System.Drawing.Point(175, 144);
+            this.btnQitarSelec.Name = "btnQitarSelec";
+            this.btnQitarSelec.Size = new System.Drawing.Size(62, 23);
+            this.btnQitarSelec.TabIndex = 1;
+            this.btnQitarSelec.Text = "<";
+            this.btnQitarSelec.UseVisualStyleBackColor = true;
+            this.btnQitarSelec.Click += new System.EventHandler(this.btnQitarSelec_Click);
+            // 
+            // btnAgregarAll
+            // 
+            this.btnAgregarAll.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAgregarAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarAll.Location = new System.Drawing.Point(175, 56);
+            this.btnAgregarAll.Name = "btnAgregarAll";
+            this.btnAgregarAll.Size = new System.Drawing.Size(62, 23);
+            this.btnAgregarAll.TabIndex = 1;
+            this.btnAgregarAll.Text = ">>";
+            this.btnAgregarAll.UseVisualStyleBackColor = true;
+            this.btnAgregarAll.Click += new System.EventHandler(this.btnAgregarAll_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(307, 307);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnAgregarSelec
+            // 
+            this.btnAgregarSelec.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarSelec.Location = new System.Drawing.Point(175, 100);
+            this.btnAgregarSelec.Name = "btnAgregarSelec";
+            this.btnAgregarSelec.Size = new System.Drawing.Size(62, 23);
+            this.btnAgregarSelec.TabIndex = 1;
+            this.btnAgregarSelec.Text = ">";
+            this.btnAgregarSelec.UseVisualStyleBackColor = true;
+            this.btnAgregarSelec.Click += new System.EventHandler(this.btnAgregarSelec_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 371);
+            this.Controls.Add(this.btnAgregarSelec);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAgregarAll);
+            this.Controls.Add(this.btnQitarSelec);
+            this.Controls.Add(this.btnQuitarAll);
+            this.Controls.Add(this.lstSlectMeses);
+            this.Controls.Add(this.lstMeses);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "colecciones";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMeses;
+        private System.Windows.Forms.ListBox lstSlectMeses;
+        private System.Windows.Forms.Button btnQuitarAll;
+        private System.Windows.Forms.Button btnQitarSelec;
+        private System.Windows.Forms.Button btnAgregarAll;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAgregarSelec;
+    }
+}
+
---
+++ ejercicio 2 meses/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_2_meses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lstMeses.Items.Clear();
+            this.lstMeses.Items.Add("Enero");
+            this.lstMeses.Items.Add("Febrero");
+            this.lstMeses.Items.Add("Marzo");
+            this.lstMeses.Items.Add("Abril");
+            this.lstMeses.Items.Add("Mayo");
+            this.lstMeses.Items.Add("Junio");
+            this.lstMeses.Items.Add("Julio");
+            this.lstMeses.Items.Add("Agosto");
+            this.lstMeses.Items.Add("Septiembre");
+            this.lstMeses.Items.Add("Octubre");
+            this.lstMeses.Items.Add("Noviembre");
+            this.lstMeses.Items.Add("Diciembre");
+
+        }
+        
+	
+
+
+        private void lstMeses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnAgregarSelec_Click(object sender, EventArgs e)
+        {
+            if (!(this.lstMeses.SelectedItem is null))
+            {
+                this.lstSlectMeses.Items.Add(this.lstMeses.SelectedItem);
+                this.lstMeses.Items.Remove(this.lstMeses.SelectedItem);
+
+            }
+            else
+            {
+                MessageBox.Show("seleccione un elemento");
+               
+            }
+                    
+            
+
+        }
+
+        private void btnQitarSelec_Click(object sender, EventArgs e)
+        {
+            if (this.lstSlectMeses.SelectedItem is null)
+            {
+                MessageBox.Show("seleccione un elemento");
+            }
+            else
+            {
+
+                this.lstMeses.Items.Add(this.lstSlectMeses.SelectedItem);
+                this.lstSlectMeses.Items.Remove(this.lstSlectMeses.SelectedItem);
+            }
+        }
+
+        private void btnAgregarAll_Click(object sender, EventArgs e)
+        {
+            this.lstSlectMeses.Items.AddRange(this.lstMeses.Items);
+            this.lstMeses.Items.Clear();
+        }
+
+        private void btnQuitarAll_Click(object sender, EventArgs e)
+        {
+            this.lstMeses.Items.AddRange(this.lstSlectMeses.Items);
+            this.lstSlectMeses.Items.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
